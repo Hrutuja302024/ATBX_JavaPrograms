8fe2f0208a1432d81725f5bd7e1867c37906c4f9
Hrutuja <harkehrutuja17@gmail.com>
2024-11-21 19:56:10 +0530

    hi

---
+++ src/Lab003.java
@@ -0,0 +1,13 @@
+public class Lab003 {
+    int a=5;
+    int b=4;
+    //int c=a+b;
+    public static void main(String[] args) {
+        System.out.println("c=a+b");
+        System.out.println(10+3);  //addition
+        System.out.println(10-3);  //subtraction
+        System.out.println(10*3);  //multiplication
+        System.out.println(10/3); //division
+
+    }
+}
